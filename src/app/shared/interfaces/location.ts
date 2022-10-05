a59241c09c0c1010a25481671b5a654ee4e89399
Pablo <pablo.pita@tokiota.com>
2022-10-05 12:47:34 +0200

    location model

---
+++ src/app/shared/interfaces/location.ts
@@ -0,0 +1,20 @@
+export interface Location {
+  latitude: number;
+  longitude: number;
+  type: string;
+  name: string;
+  number: string;
+  postal_code: string;
+  street: string;
+  confidence: number;
+  region: string;
+  region_code: string;
+  county: string;
+  locality: string;
+  administrative_area: null;
+  neighbourhood: string;
+  country: string;
+  country_code: string;
+  continent: string;
+  label: string;
+}
